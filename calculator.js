feeeeb4d2489ec3e4fc399bf3df9f962e1285bc1
C4mz0r <aut0x@hotmail.com>
2015-12-23 20:15:11 -0700

    Implement calculator logic

--- calculator.js
+++ calculator.js
@@ -22,20 +22,30 @@ function evaluate(buffer) {
 	return eval(stringBuffer);
 }
 
-function updateExpression(buffer) {
-	$('.expression').text(buffer);
-}
-
 function updateDisplay(buffer) {
 	$('.screen').text(buffer);
 }
 
+var allowableNumbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+var allowableOperators = ['+', '-', '*', '/', '='];
+
 $(document).ready( function(){
+
+	// help us know if we are dealing with the left operand,
+	// the operator, or the right operand.
+	const left = 1, op = 2, right = 3;
+	const clear = "Clear";
 
 	// Hold the key presses	
 	var buffer = [];
-	var screenDisplay = [];
+	var currentNumberBuffer = [];
 	var justEvaluated = false;	
+	var currentState = left;
+	
+	var leftHandSide = "";
+	var operator = "";
+	var rightHandSide = "";
+	var shouldClearLeftHandSide = false;
 		
 	// "Listen" to the keys that are pressed and store in the buffer.
 	// When the equal button is pressed, then the buffer gets evaluated.
@@ -43,53 +53,103 @@ $(document).ready( function(){
 	$("div").click(function(){				
 	
 		var pressed = $(this).context.textContent;
-		switch( pressed )
-		{
-			case "1":
-			case "2":
-			case "3":
-			case "4":
-			case "5":
-			case "6":
-			case "7":
-			case "8":
-			case "9":
-			case "0":
-			   			
-			   	// If the previous answer is showing,
-			   	// typing a new number should start things
-			   	// over fresh.
-				if (justEvaluated === true) {
-				  buffer = [pressed];
-				  justEvaluated = false;
-				  break;
-				}
-								
-			case "+":
-			case "-":
-			case "*":
-			case "/":
+		console.log("Detected: " + pressed);
+		if (currentState === left) {
 			
+			// is the pressed key a number?
+			// if so, then 'grow' the current number
+			if ( allowableNumbers.indexOf(pressed) >= 0 ) {				
+				if (shouldClearLeftHandSide === true) {
+					leftHandSide = "";
+					shouldClearLeftHandSide = false;
+				}				
+				leftHandSide += pressed;
+				updateDisplay(leftHandSide);				
+			} else if (allowableOperators.indexOf(pressed) >= 0) {
+				operator = pressed;
+				currentState = op;
+			} else if (pressed === clear) {
+				currentState = left;
+				leftHandSide = [];
+				rightHandSide = [];
+				operator = "";
+				updateDisplay(leftHandSide);
+			}
+			
+			 
+		}
+	    
+	    else if ( currentState === op || currentState === right ) {
+			if ( allowableNumbers.indexOf(pressed) >= 0 ) {
+				rightHandSide += pressed;
+				updateDisplay(rightHandSide);
+			} else if (allowableOperators.indexOf(pressed) >= 0) {
+				// evaluate LHS ("old" operator) RHS and store in LHS,
+				// then clear out RHS.
 				
-				buffer[buffer.length] = $(this).context.textContent;
-				justEvaluated = false;
-				break;		
-							
-			case "=":
-				// Evaluate
-				buffer = [evaluate(buffer)];
-				justEvaluated = true;						
-				break;
+				x = parseFloat(leftHandSide);
+				y = parseFloat(rightHandSide);
 				
-			case "Clear":
-			  	buffer = [];			  				  
-				break;
-			  					
+				// Calculate if the operands are not NaN:
+				if ( !isNaN(x) && !isNaN(y) )
+				{
+					
+					switch(operator) {					
+						case '+': 
+							leftHandSide = add(x, y).toString();
+							rightHandSide = "";								
+							break;
+						
+						case '-':
+							leftHandSide = subtract(x, y).toString();
+							rightHandSide = "";
+							break;
+						
+						case '*':
+							leftHandSide = multiply(x, y).toString();
+							rightHandSide = "";
+							break;
+						
+						case '/': 
+							leftHandSide = divide(x, y).toString();
+							rightHandSide = "";
+							break;
+					}
+				
+					updateDisplay(leftHandSide);
+				
+				}
+				
+				if (pressed === '=') {
+					// after the user presses equals, then the next input
+					// would be the LHS operator, and it should be flagged to clear out
+					// when the user starts typing it next time.
+					// We don't clear it directly now, since we want to allow the user to possibly type another
+					// operator, such as '+' and then the next RHS number.
+					currentState = left;
+					operator = "";
+					shouldClearLeftHandSide = true;
+				}
+				else {				
+					currentState = op;				
+					operator = pressed;
+				}
+				
+				
+			} else if (pressed === clear) {
+				currentState = left;
+				leftHandSide = [];
+				rightHandSide = [];
+				operator = "";
+				updateDisplay(leftHandSide);
+			}
 		}
 		
-		
-		console.log(buffer);
-	;})
+			
+		console.log("LHS is now: " + leftHandSide.toString());
+		console.log("Operator is now: " + operator);
+		console.log("RHS is now: " + rightHandSide.toString());
+	});
 	
 		
 });	    
